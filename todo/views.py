9e75ba820355e7260e668c4bd913dd922c220f8c
ryo0705s <inspire.sasaki15@gmail.com>
2022-03-27 01:48:21 +0900

    crud機能搭載完了

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Todo
 # from django.http import HttpResponse
 # Create your views here.
@@ -10,5 +11,20 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
   model = Todo
   context_object_name = "task"
+
+class TodoCreate(CreateView):
+  model = Todo
+  fields = "__all__"
+  success_url = reverse_lazy("list")
+  
+class TodoUpdate(UpdateView):
+  model = Todo
+  fields = "__all__"
+  success_url = reverse_lazy("list")
+  
+class TodoDelete(DeleteView):
+  model = Todo
+  context_object_name = "task"
+  success_url = reverse_lazy("list")
 # def index(request):
 #   return HttpResponse("<H1>Hello World</H1>")
